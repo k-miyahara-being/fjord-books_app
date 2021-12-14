80b84cade20bf905576c4867a28e85eedae2a690
k-miyahara-being <k-miyahara@beingcorp.co.jp>
2021-12-14 13:03:51 +0900

    テストコードの追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable' do
+    my_report = reports(:alice)
+    user01 = users(:alice)
+    user02 = users(:bob)
+
+    assert my_report.editable?(user01)
+    assert_not my_report.editable?(user02)
+  end
+
+  test '#created_on' do
+    my_report = reports(:alice)
+
+    assert_equal Date.current, my_report.created_on
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,50 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#following' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+
+    me.follow(she)
+    assert_equal true, me.following?(she)
+    me.unfollow(she)
+    assert_equal false, me.following?(she)
+  end
+
+  test '#followed_by' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+
+    me.follow(she)
+    assert_equal true, she.followed_by?(me)
+    me.unfollow(she)
+    assert_equal false, she.following?(me)
+  end
+
+  test '#follow' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+
+    assert_not me.following?(she)
+    me.follow(she)
+    assert me.following?(she)
+  end
+
+  test '#unfollow' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+
+    me.follow(she)
+    assert me.following?(she)
+    me.unfollow(she)
+    assert_not me.following?(she)
+  end
+
+  test '#name_or_email' do
+    user = User.new(email: 'foo@example.com', name: '')
+    assert_equal 'foo@example.com', user.name_or_email
+
+    user.name = 'Foo Bar'
+    assert_equal 'Foo Bar', user.name_or_email
+  end
 end
